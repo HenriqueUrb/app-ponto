92f2d1a7b35884e06cb5240a9c61fe61457e8cbe
Henrique <henrique.urbano2002@hotmail.com>
2023-11-29 13:13:30 -0300

    Mensagem descritiva do commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss']
 })
-export class HomePage {
+export class HomePage implements OnDestroy {
+  horaAtual!: Date;
+  private timerSubscription: Subscription;
 
-  constructor() {}
+  constructor() {
+    // Atualiza a hora a cada segundo
+    this.timerSubscription = interval(1000).subscribe(() => {
+      this.atualizarHora();
+    });
+  }
 
+  ngOnDestroy() {
+    // Cancela a assinatura do timer ao sair da página para evitar vazamentos de memória
+    if (this.timerSubscription) {
+      this.timerSubscription.unsubscribe();
+    }
+  }
+
+  private atualizarHora() {
+    this.horaAtual = new Date();
+  }
 }
